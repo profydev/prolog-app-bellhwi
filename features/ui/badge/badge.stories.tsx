7ab630cbb6ee1cc4afe7737cadb46c928fb74e31
Jonghwi Kim <54308894+bellhwi@users.noreply.github.com>
2023-01-19 15:21:30 -0800

    Landing page header (#16)
    * Fix navigation logo when switching to mobile
    
    * Fix merge conflict
    
    * Lading page header
    
    * Landing page header finish

--- features/ui/badge/badge.stories.tsx
+++ features/ui/badge/badge.stories.tsx
@@ -1,90 +1,7 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Badge, BadgeSize, BadgeColor } from "./badge";
-import styled from "styled-components";
-import { color, displayFont, textFont } from "@styles/theme";
 
-type PositionType = {
-  top: string;
-  left: string;
-};
-
-type SubContainerType = {
-  top: string;
-  height: string;
-};
-
-const Container = styled.div`
-  position: relative;
-  width: 1114px;
-  height: 1293px;
-`;
-const HeadingContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 0px;
-  position: absolute;
-  width: 1024px;
-  height: 152px;
-  left: 57px;
-  top: 110px;
-`;
-
-const SubContainer = styled.div<SubContainerType>`
-  position: absolute;
-  width: 960px;
-  left: 57px;
-  top: ${(props) => props.top};
-  height: ${(props) => props.height};
-`;
-
-const Title = styled.h1`
-  ${displayFont("xl", "semibold")};
-  color: ${color("gray", 900)};
-  margin: 0;
-`;
-
-const Subtitle = styled.p`
-  ${textFont("xl", "regular")};
-  font-size: 20px;
-  line-height: 30px;
-  color: ${color("gray", 500)};
-  margin: 20px 0 0 0;
-  letter-spacing: 0.02em;
-`;
-
-const Heading = styled.h1`
-  ${displayFont("xs", "medium")};
-  color: ${color("gray", 900)};
-  margin: 0;
-  letter-spacing: 0.02em;
-`;
-
-const Description = styled.p`
-  ${textFont("lg", "regular")};
-  color: ${color("gray", 500)};
-  margin: 12px 0 0 0;
-  letter-spacing: 0.02em;
-`;
-
-const Label = styled.span<PositionType>`
-  position: absolute;
-  text-align: center;
-  left: ${(props) => props.left};
-  top: ${(props) => props.top};
-  ${textFont("md", "regular")};
-  color: ${color("gray", 500)};
-  width: 70px;
-  height: 21px;
-  letter-spacing: 0.02em;
-`;
-
-const BadgeContainer = styled.div<PositionType>`
-  position: absolute;
-  left: ${(props) => props.left};
-  top: ${(props) => props.top};
-`;
 export default {
   title: "UI/Badge",
   component: Badge,
@@ -117,7 +34,7 @@ const Template: ComponentStory<typeof Badge> = ({ size, color, icon }) => (
 
 export const Default = Template.bind({});
 Default.args = {
-  size: BadgeSize.md,
+  size: BadgeSize.sm,
   color: BadgeColor.primary,
 };
 Default.parameters = {
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,56 @@
 import styled from "styled-components";
 import { Routes } from "@config/routes";
+import { color, textFont, displayFont } from "@styles/theme";
+import { Testimonial } from "@features/ui";
+
+const menuItems = [
+  { text: "Home", href: Routes.home },
+  { text: "Products", href: Routes.products },
+  { text: "Documentation", href: Routes.documentation },
+  { text: "Pricing", href: Routes.pricing },
+];
+
+const companyLogosURL = [
+  "company-logos/layers.svg",
+  "company-logos/sisyphus.svg",
+  "company-logos/circooles.svg",
+  "company-logos/catalog.svg",
+  "company-logos/quotient.svg",
+];
+
+const testimonials = [
+  {
+    primary: true,
+    heading: "Frontend Development",
+    content:
+      "Prolog has saved us many times. We get an alert, investigate the error, and fix it. That simple.",
+    avatar: "avatars/mollie.svg",
+    author: "Mollie Hall",
+    company: "Web Developer, Sisyphus",
+  },
+  {
+    heading: "Microservice Architectures",
+    content:
+      "Our services fail from time to time. That’s normal. But with Prolog we’re able to track the issue down in no time. ",
+    avatar: "avatars/alec.svg",
+    author: "Alec Whitten",
+    company: "Software Architect, Layers",
+  },
+  {
+    primary: true,
+    heading: "Backend Servers",
+    content:
+      "Prolog’s UI is beautiful and intuitive. It’s simple to find bugs and our devs are always on top of pressing issues.",
+    avatar: "avatars/kelly.svg",
+    author: "Kelly Williams",
+    company: "Engineering Manager, Catalog",
+  },
+];
 
 const Header = styled.header`
   width: 100%;
   height: 80px;
-  padding: 0 2rem;
+  padding: 0 7rem;
   box-sizing: border-box;
   display: flex;
   justify-content: space-between;
@@ -12,10 +58,84 @@ const Header = styled.header`
   background: white;
 `;
 
+const MenuContainer = styled.ul`
+  list-style: none;
+  display: flex;
+  margin-right: 4rem;
+`;
+
+const MenuList = styled.li`
+  &:not(:first-child) {
+    padding-left: 2rem;
+  }
+`;
+
+const MenuLink = styled.a`
+  text-decoration: none;
+  color: ${color("gray", 500)};
+  ${textFont("md", "medium")};
+`;
+
+const Hero = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${color("gray", 50)};
+  padding-bottom: 96px;
+`;
+
+const Heading = styled.h1`
+  ${displayFont("xl", "semibold")};
+  color: ${color("gray", 900)};
+  letter-spacing: -0.02em;
+  padding-left: 8px;
+`;
+
+const Text = styled.p`
+  text-align: center;
+  color: ${color("gray", 500)};
+  ${textFont("xl", "regular")};
+  padding-left: 12px;
+`;
+
+const TextSmall = styled.p`
+  text-align: center;
+  color: ${color("gray", 500)};
+  ${textFont("md", "medium")};
+  margin: 0px;
+`;
+
+const SocialSection = styled.div`
+  padding: 96px 0px;
+  background-color: ${color("gray", 50)};
+`;
+
+const CompanyLogos = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 1216px;
+  margin: 32px auto 0px;
+  padding-left: 12px;
+`;
+
+const TestimonialSection = styled.div`
+  text-align: center;
+  background-color: white;
+`;
+
+const Testimonials = styled.div`
+  display: flex;
+  padding: 0px 93.5px;
+  margin: 64px 0px 96px;
+  height: 416px;
+`;
+
 const ContactButton = styled.button`
-  position: absolute;
+  position: fixed;
   bottom: 2.5rem;
-  right: 2.5rem;
+  right: 1.5rem;
   padding: 1rem;
   background: #7f56d9;
   border-radius: 50%;
@@ -34,8 +154,51 @@ const IssuesPage = () => {
       <Header>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
+        <MenuContainer>
+          {menuItems.map((menuItem, index) => (
+            <MenuList key={index}>
+              <MenuLink href={menuItem.href}>{menuItem.text}</MenuLink>
+            </MenuList>
+          ))}
+        </MenuContainer>
         <a href={Routes.projects}>Dashboard</a>
       </Header>
+      <Hero>
+        <Heading style={{ margin: "96px 0px 0px" }}>
+          Your Issues In Sight. At All Times.
+        </Heading>
+        <Text style={{ width: "648px", margin: "24px 0px 64px" }}>
+          Powerful error tracking and monitoring for software applications.
+          Trusted by over 4,000 startups.
+        </Text>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={"images/landing-macbook.svg"}
+          style={{ marginLeft: "12px" }}
+        />
+      </Hero>
+      <SocialSection>
+        <TextSmall>Join 4,000+ companies using Prolog</TextSmall>
+        <CompanyLogos>
+          {companyLogosURL.map((companyLogo, index) => (
+            /* eslint-disable-next-line @next/next/no-img-element */
+            <img key={index} src={companyLogo} />
+          ))}
+        </CompanyLogos>
+      </SocialSection>
+      <TestimonialSection>
+        <Heading style={{ margin: "96px 0px 0px" }}>
+          Don’t Only Trust Our Words
+        </Heading>
+        <Text style={{ marginBottom: "0px" }}>
+          Our customers around the globe share their opinions.
+        </Text>
+        <Testimonials>
+          {testimonials.map((testimonial, index) => (
+            <Testimonial {...testimonial} key={index}></Testimonial>
+          ))}
+        </Testimonials>
+      </TestimonialSection>
       <ContactButton
         onClick={() =>
           alert(
